df33545116d67ef10cb032c7e03093b6c6206e3b
Spectro <mateusguilherme348@gmail.com>
2026-09-14 17:17:11 -0300

    Spectro

--- item/formulario.php
+++ item/formulario.php
@@ -1,19 +1,19 @@
-<div class="container border my-3" >
-    <form  method="post">
-    <div class="row">
-        <div class="col-3">Nome:</div>
-        <div class="col"><input type="text" name="nome" class="form-control" required></div>
-    </div>
-    <div class="row">
-        <div class="col-3">Descrição:</div>
-        <div class="col"><input type="text" name="descricao"class="form-control"></div>
-    </div>
-    <div class="row">
-        <div class="col-3">Patrimonio:</div>
-        <div class="col"><input type="text" name="patrimonio" class="form-control" requerid></div>
-    </div>
-    <div class="row">
-        <div class="col text-center"><button class="btn btn-success " >Cadastrar</button></div>
-    </div>
-</form>
-    </div>
+<div class="container my-3">
+    <form method="post" class="form-control">
+        <div class="row">
+            <div class="col-3">Nome:</div>
+            <div class="col"><input name="nome" type="text" class="form-control" required></div>
+        </div>
+        <div class="row">
+            <div class="col-3">Descrição:</div>
+            <div class="col"><input name="descricao" type="text" class="form-control"></div>
+        </div>
+        <div class="row">
+            <div class="col-3">Patrimonio:</div>
+            <div class="col"><input name="patrimonio" type="text" class="form-control" required></div>
+        </div>
+        <div class="row">
+            <div class="col text-center"><button class="btn btn-success">Cadastrar</button></div>
+        </div>
+    </form>
+</div>
